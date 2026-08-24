0ada62e3eff0fb1401c03f38037418f05ab80099
therubento <rmdaqb243@gmail.com>
2026-08-25 00:38:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const projetos = [
         categoria: "AUDIOVISUAL",
         hashtag: "#documentario",
         titulo: "Sala 31 / 30 Anos de CC",
-        mesAno: "2026",
+        mesAno: "Fevereiro 2025 - Atualidade",
         cargo: "Realizador",
         local: "Universidade do Algarve",
         capa: "https://images.unsplash.com/photo-1485846234645-a62644f84728?auto=format&fit=crop&w=600&q=80",
